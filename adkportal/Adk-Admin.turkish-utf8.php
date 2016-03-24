1bc51a84a6d99bd6bc0e081f48211c22bd2956e2
CeeMoo <qdale.123@gmail.com>
2016-03-24 16:55:08 +0200

    admin

---
+++ adkportal/Adk-Admin.turkish-utf8.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Adk Portal
+ * Version: 3.0
+ * Official support: http://www.smfpersonal.net
+ * Author: Adk Team
+ * Copyright: 2009 - 2014 © SMFPersonal
+ * Developers:
+ * 		Juarez, Lucas Javier
+ * 		Clavijo, Pablo
+ *
+ * version smf 2.0*
+ *tükçe çeviri: http://smf.konusal.com/
+ */
+
+//Sources File
+$txt['adkadmin_name'] = 'Adk Portal';
+$txt['adkadmin_settings'] = 'Portal Ayarları';
+$txt['adkadmin_news_desc'] = 'Adk Portal en son haberleri göster.';
+$txt['adkadmin_news'] = 'Haberler';
+$txt['adkadmin_setting_desc'] = 'Adk-Portal Genel Seçenekler yönetin.';
+$txt['adkadmin_setting'] = 'Seçenekler';
+$txt['adkadmin_icons'] = 'Simgeler';
+$txt['adkadmin_icons_desc'] = 'portalınızda simgeleri yönetin. simgeler blok başlıklarında görünür.';
+$txt['adkadmin_download_now'] = 'İndiriliyor';
+$txt['adkadmin_stand_desc'] = 'Bağımsız modda portal yapılandırmasını değiştirmek';
+$txt['adkadmin_stand'] = 'Bağımsız ayarları';
+$txt['adkadmin_smf_personal'] = 'Adk Portal yardım';
+
+
+//Template File
+$txt['adkadmin_news_smfp'] = 'SMFPersonal.net dan En Son Haberler';
+$txt['adkadmin_yourversion'] = 'Sizin Sürüm';
+$txt['adkadmin_currentversion'] = 'Son Sürüm';
+$txt['adkadmin_general_settings'] = 'Genel Ayarlar';
+$txt['adkadmin_columns_settings'] = 'Sütun Ayarları';
+$txt['adkadmin_more_settings'] = 'Daha fazla ayar';
+$txt['adkadmin_enable_img_blocks'] = 'bloklarda simgeleri etkin hale getir.';
+$txt['adkadmin_mofe_info_stand'] = 'To account for: You need to upload the file AdkStandAlone.php in place that corresponds to this url. Eğer şüpheniz varsa, SMF Personalın ekibine başvurun çekinmeyin.
+Once uploaded the file, rename to index.php';
+$txt['adkadmin_disable'] = 'Devre dışı';
+$txt['adkadmin_enable'] = 'Etkin';
+$txt['adkadmin_stand'] = 'Bağımsız';
+$txt['adkadmin_url_stand'] = 'Bağımsız mod URL';
+$txt['adkadmin_change_title'] = 'Değiştir portal başlıklarını.';
+$txt['adkadmin_hide_version'] = 'Adk portal versiyonu gizle ';
+$txt['adkadmin_guest_view_post'] = 'Özel modunda göster Siteyi';
+$txt['adkadmin_linktree_portal'] = 'Portalda linktree devre dışı bırak';
+$txt['adkadmin_width_portal'] = 'Sütun genişlikleri';
+$txt['adkadmin_right'] = 'Sağ';
+$txt['adkadmin_left'] = 'Sol';
+$txt['adkadmin_disable_colexpand'] = 'Sütunları genişletip daraltmayı gizle';
+$txt['adkadmin_format_blocks'] = 'Portalınız için bir stil seçin';
+$txt['adkadmin_roundFrame'] = 'Kavisli Çerçeve';
+$txt['adkadmin_inblocks'] = 'blok içindeki Başlık';
+$txt['adkadmin_catbg'] = 'Catbg';
+$txt['adkadmin_titlebg'] = 'Titlebg';
+$txt['adkadmin_windowbg'] = 'Windowbg';
+$txt['adkadmin_alternative'] = 'Alternatif';
+$txt['adkadmin_teu'] = 'Tam bir sütun';
+$txt['adkportal_activate'] = 'Adk Portal Durumu';
+$txt['adkportal_delete_icon'] = 'Bu simgeyi kaldırmak istediğinizden emin misiniz? Bu işlem geri alınamaz.';
+$txt['adkportal_delete'] = 'Sil';
+$txt['adkportal_add_icon'] = 'Simge Ekle';
+$txt['adkportal_select_a_icon'] = 'Bir simge seçin';
+$txt['adkadmin_include_ssi'] = 'Include SSI.php file';
+
+//AdkSeo.php is a short file... It does not need a custom file
+$txt['adkportal_htaccess_info'] = 'Burdan Url yapısını değiştirebilirsiniz.Url yapısı değişikliği: <strong>Adk Portal Sayfa Sistemi - Download Sistemi</strong> etkiler. Zaten bir htaccess varsa alttaki kodu eklemeniz gerekir';
+$txt['adkportal_htaccess_moreinfo'] = 'Eğer herhangi bir htaccess yoksa, hiç bir bilgi aşağıdaki tabloda olmayacaktır.<br />Sadece <strong>Kaydet</strong>  tıklayın ve sistem otomatik olarak yeni bir htaccess yaratacak.';
+$txt['adkportal_seo_use_path'] = 'Forum kök dizinde değil herhangi bir klasör üzerinde ise';
+$txt['adkportal_seo_enable_pages'] = 'Sayfa URL\'leri sef url çevir?';
+$txt['adkportal_seo_enable_downloads'] = 'Yüklemeler ve Kategoriler URLlerini sef url çevir?';
+$txt['adkportal_robots'] = 'Burada robots.txt oluşturmak mümkündür.';
+$txt['adkportal_robots_info'] = 'Hatırlatma:Robots.txt dosyası sitenizin kök dizinine yerleştirilmelidir.Bu robots.txt forumlarda kök dizinde oluşturulur.Web Sitesi kök dizini başka bir yerde barındırılıyor ise oluşturulduktan sonra robots.txt dosyasını kopyalamanız gerekir.';
+$txt['adkportal_seo_htaccess_info'] = 'Herhangi bir değişiklik yapmadan önce, buradan htaccess dosyası oluşturmanız gerekir.';
+$txt['adkportal_seo_manage_desc'] = 'Seo değişikliklerini yönet ';
+$txt['adkportal_create_robots'] = 'Robots.txt Dosyası Oluştur';
+$txt['adkportal_delete_htaccess'] = 'Htaccess Sil';
+
+//Post install
+$txt['adk_post_install'] = 'Adk Portal yüklendi';
+$txt['adk_post_install_2'] = 'Tablolar oluşturuldu';
+$txt['adk_post_install_3'] = 'Sütunlar oluşturuldu';
+$txt['adk_post_install_4'] = 'Sütun {$name_column} üzerinde {$name_table}';
+
+?>
---
+++ adkportal/Adk-Admin.turkish.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Adk Portal
+ * Version: 3.0
+ * Official support: http://www.smfpersonal.net
+ * Author: Adk Team
+ * Copyright: 2009 - 2014 © SMFPersonal
+ * Developers:
+ * 		Juarez, Lucas Javier
+ * 		Clavijo, Pablo
+ *
+ * version smf 2.0*
+ *tükçe çeviri: http://smf.konusal.com/
+ */
+
+//Sources File
+$txt['adkadmin_name'] = 'Adk Portal';
+$txt['adkadmin_settings'] = 'Portal Ayarlarý';
+$txt['adkadmin_news_desc'] = 'Adk Portal en son haberleri göster.';
+$txt['adkadmin_news'] = 'Haberler';
+$txt['adkadmin_setting_desc'] = 'Adk-Portal Genel Seçenekler yönetin.';
+$txt['adkadmin_setting'] = 'Seçenekler';
+$txt['adkadmin_icons'] = 'Simgeler';
+$txt['adkadmin_icons_desc'] = 'portalýnýzda simgeleri yönetin. simgeler blok baþlýklarýnda görünür.';
+$txt['adkadmin_download_now'] = 'Ýndiriliyor';
+$txt['adkadmin_stand_desc'] = 'Baðýmsýz modda portal yapýlandýrmasýný deðiþtirmek';
+$txt['adkadmin_stand'] = 'Baðýmsýz ayarlarý';
+$txt['adkadmin_smf_personal'] = 'Adk Portal yardým';
+
+
+//Template File
+$txt['adkadmin_news_smfp'] = 'SMFPersonal.net dan En Son Haberler';
+$txt['adkadmin_yourversion'] = 'Sizin Sürüm';
+$txt['adkadmin_currentversion'] = 'Son Sürüm';
+$txt['adkadmin_general_settings'] = 'Genel Ayarlar';
+$txt['adkadmin_columns_settings'] = 'Sütun Ayarlarý';
+$txt['adkadmin_more_settings'] = 'Daha fazla ayar';
+$txt['adkadmin_enable_img_blocks'] = 'bloklarda simgeleri etkin hale getir.';
+$txt['adkadmin_mofe_info_stand'] = 'To account for: You need to upload the file AdkStandAlone.php in place that corresponds to this url. Eðer þüpheniz varsa, SMF Personalýn ekibine baþvurun çekinmeyin.
+Once uploaded the file, rename to index.php';
+$txt['adkadmin_disable'] = 'Devre dýþý';
+$txt['adkadmin_enable'] = 'Etkin';
+$txt['adkadmin_stand'] = 'Baðýmsýz';
+$txt['adkadmin_url_stand'] = 'Baðýmsýz mod URL';
+$txt['adkadmin_change_title'] = 'Deðiþtir portal baþlýklarýný.';
+$txt['adkadmin_hide_version'] = 'Adk portal versiyonu gizle ';
+$txt['adkadmin_guest_view_post'] = 'Özel modunda göster Siteyi';
+$txt['adkadmin_linktree_portal'] = 'Portalda linktree devre dýþý býrak';
+$txt['adkadmin_width_portal'] = 'Sütun geniþlikleri';
+$txt['adkadmin_right'] = 'Sað';
+$txt['adkadmin_left'] = 'Sol';
+$txt['adkadmin_disable_colexpand'] = 'Sütunlarý geniþletip daraltmayý gizle';
+$txt['adkadmin_format_blocks'] = 'Portalýnýz için bir stil seçin';
+$txt['adkadmin_roundFrame'] = 'Kavisli Çerçeve';
+$txt['adkadmin_inblocks'] = 'blok içindeki Baþlýk';
+$txt['adkadmin_catbg'] = 'Catbg';
+$txt['adkadmin_titlebg'] = 'Titlebg';
+$txt['adkadmin_windowbg'] = 'Windowbg';
+$txt['adkadmin_alternative'] = 'Alternatif';
+$txt['adkadmin_teu'] = 'Tam bir sütun';
+$txt['adkportal_activate'] = 'Adk Portal Durumu';
+$txt['adkportal_delete_icon'] = 'Bu simgeyi kaldýrmak istediðinizden emin misiniz? Bu iþlem geri alýnamaz.';
+$txt['adkportal_delete'] = 'Sil';
+$txt['adkportal_add_icon'] = 'Simge Ekle';
+$txt['adkportal_select_a_icon'] = 'Bir simge seçin';
+$txt['adkadmin_include_ssi'] = 'Include SSI.php file';
+
+//AdkSeo.php is a short file... It does not need a custom file
+$txt['adkportal_htaccess_info'] = 'Burdan Url yapýsýný deðiþtirebilirsiniz.Url yapýsý deðiþikliði: <strong>Adk Portal Sayfa Sistemi - Download Sistemi</strong> etkiler. Zaten bir htaccess varsa alttaki kodu eklemeniz gerekir';
+$txt['adkportal_htaccess_moreinfo'] = 'Eðer herhangi bir htaccess yoksa, hiç bir bilgi aþaðýdaki tabloda olmayacaktýr.<br />Sadece <strong>Kaydet</strong>  týklayýn ve sistem otomatik olarak yeni bir htaccess yaratacak.';
+$txt['adkportal_seo_use_path'] = 'Forum kök dizinde deðil herhangi bir klasör üzerinde ise';
+$txt['adkportal_seo_enable_pages'] = 'Sayfa URL\'leri sef url çevir?';
+$txt['adkportal_seo_enable_downloads'] = 'Yüklemeler ve Kategoriler URLlerini sef url çevir?';
+$txt['adkportal_robots'] = 'Burada robots.txt oluþturmak mümkündür.';
+$txt['adkportal_robots_info'] = 'Hatýrlatma:Robots.txt dosyasý sitenizin kök dizinine yerleþtirilmelidir.Bu robots.txt forumlarda kök dizinde oluþturulur.Web Sitesi kök dizini baþka bir yerde barýndýrýlýyor ise oluþturulduktan sonra robots.txt dosyasýný kopyalamanýz gerekir.';
+$txt['adkportal_seo_htaccess_info'] = 'Herhangi bir deðiþiklik yapmadan önce, buradan htaccess dosyasý oluþturmanýz gerekir.';
+$txt['adkportal_seo_manage_desc'] = 'Seo deðiþikliklerini yönet ';
+$txt['adkportal_create_robots'] = 'Robots.txt Dosyasý Oluþtur';
+$txt['adkportal_delete_htaccess'] = 'Htaccess Sil';
+
+//Post install
+$txt['adk_post_install'] = 'Adk Portal yüklendi';
+$txt['adk_post_install_2'] = 'Tablolar oluþturuldu';
+$txt['adk_post_install_3'] = 'Sütunlar oluþturuldu';
+$txt['adk_post_install_4'] = 'Sütun {$name_column} üzerinde {$name_table}';
+
+?>
