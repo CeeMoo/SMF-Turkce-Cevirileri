dc22de8411928a79ffbcaf2910718827f4f73c8a
osman <snrj_od_@hotmail.com>
2016-03-24 21:15:25 +0200

    Tamamlandı Gözden geçirilmeli

--- adkportal/Adk-Downloads.turkish-utf8.php
+++ adkportal/Adk-Downloads.turkish-utf8.php
@@ -14,56 +14,56 @@
  */
 
 //Subs txt
-$txt['adkdown_approve'] = 'Approve';
-$txt['adkdown_unapprove'] = 'Unapprove';
+$txt['adkdown_approve'] = 'Onaylamak';
+$txt['adkdown_unapprove'] = 'Onaylarını kaldırmak';
 $txt['adkdown_link_download'] = 'Link to downloads';
-$txt['adkdown_delete'] = 'Delete';
+$txt['adkdown_delete'] = 'Sil';
 
 //Source File
-$txt['adkdown_profile'] = 'Profile of %1$s';
-$txt['adkdown_add'] = 'Add download';
-$txt['adkdown_editing'] = 'Editing download';
-$txt['adkdown_never'] = 'Never';
-$txt['adkdown_send_pm'] = 'Has been approved';
+$txt['adkdown_profile'] = 'Profili %1$s';
+$txt['adkdown_add'] = 'Download Ekle';
+$txt['adkdown_editing'] = 'Download Düzenle';
+$txt['adkdown_never'] = 'Asla';
+$txt['adkdown_send_pm'] = 'Onaylandı';
 $txt['adkdown_view_stats'] = 'Statics';
-$txt['adkdown_search'] = 'Search';
+$txt['adkdown_search'] = 'Ara';
 $txt['adkdown_downloads'] = 'Downloads';
-$txt['adkdown_myprofile'] = 'View my profile';
-$txt['adkdown_approve_admin'] = 'Approve downloads';
-$txt['adkdown_none'] = 'Anything';
+$txt['adkdown_myprofile'] = 'Profil bilgilerim';
+$txt['adkdown_approve_admin'] = 'Downloads Onayla';
+$txt['adkdown_none'] = 'Her şey';
 
 //Template File
-$txt['adkdown_lasted_down'] = 'Lasted downloads';
-$txt['adkdown_popular'] = 'Most popular';
-$txt['adkdown_sub_category'] = 'Add Sub-category';
-$txt['adkdown_title'] = 'Title';
-$txt['adkdown_vistas'] = 'Views';
-$txt['adkdown_text_autor'] = 'Personal text';
-$txt['adkdown_title_autor'] = 'Personal title';
-$txt['adkdown_subject'] = 'Subject';
-$txt['adkdown_data_info'] = 'Download information';
-$txt['adkdown_date'] = 'Date';
-$txt['adkdown_no_downloads'] = 'No downloads';
-$txt['adkdown_category'] = 'Category';
-$txt['adkdown_last_down'] = 'Last download';
-$txt['adkdown_sub_cat'] = 'Sub-categories';
-$txt['adkdown_search_no_results'] = 'No results';
-$txt['adkdown_go_back'] = 'Go back';
-$txt['adkdown_find_results'] = 'Results';
-$txt['adkdown_author'] = 'Author';
-$txt['adkdown_send'] = 'Send';
-$txt['adkdown_unnapproved_down'] = 'This download has not been approved';
-$txt['adkdown_comment'] = 'Reply to this topic';
-$txt['adkdown_edit'] = 'Edit';
-$txt['adkdown_remove_message'] = 'Are you sure you want to delete this download? This action can not return';
-$txt['adkdown_author_info'] = 'Download author';
-$txt['adkdown_attach'] = 'Attachments';
-$txt['adkdown_view'] = 'View download';
-$txt['adkdown_not_add_more'] = 'You can not add more attachments';
-$txt['adkdown_add_more'] = 'Add another attachment';
-$txt['adkdown_screen'] = 'Image';
-$txt['adkdown_topviewed'] = 'Most viewed';
-$txt['adkdown_topdown'] = 'Most downloaded';
-$txt['adkdown_desc'] = 'Description';
+$txt['adkdown_lasted_down'] = 'Son downloads';
+$txt['adkdown_popular'] = 'En popüler';
+$txt['adkdown_sub_category'] = 'Alt kategori ekleyin';
+$txt['adkdown_title'] = 'Başlık';
+$txt['adkdown_vistas'] = 'Görüntüleme';
+$txt['adkdown_text_autor'] = 'kişisel metin';
+$txt['adkdown_title_autor'] = 'kişisel başlık';
+$txt['adkdown_subject'] = 'konu';
+$txt['adkdown_data_info'] = 'Download Bilgileri';
+$txt['adkdown_date'] = 'Tarih';
+$txt['adkdown_no_downloads'] = 'henüz hiçbir downloads';
+$txt['adkdown_category'] = 'Kategori';
+$txt['adkdown_last_down'] = 'Son download';
+$txt['adkdown_sub_cat'] = 'Alt kategoriler';
+$txt['adkdown_search_no_results'] = 'Sonuç yok';
+$txt['adkdown_go_back'] = 'Geri dön';
+$txt['adkdown_find_results'] = 'Sonuçlar';
+$txt['adkdown_author'] = 'Yazar';
+$txt['adkdown_send'] = 'Gönder';
+$txt['adkdown_unnapproved_down'] = 'Bu yükleme onaylanmadı';
+$txt['adkdown_comment'] = 'Bu konuya cevap';
+$txt['adkdown_edit'] = 'Düzenle';
+$txt['adkdown_remove_message'] = 'Bu download silmek istediğinize eminmisiniz.. bu işlem geri alınamaz';
+$txt['adkdown_author_info'] = 'Download Ekleyen';
+$txt['adkdown_attach'] = 'Eklentiler';
+$txt['adkdown_view'] = 'Download görüntüle';
+$txt['adkdown_not_add_more'] = 'Daha fazla eklenti olamaz';
+$txt['adkdown_add_more'] = 'Başka bir eklenti ekleyin';
+$txt['adkdown_screen'] = 'Resim';
+$txt['adkdown_topviewed'] = 'En çok görüntülenen';
+$txt['adkdown_topdown'] = 'En çok indirilen';
+$txt['adkdown_desc'] = 'Açıklama';
 
 ?>
